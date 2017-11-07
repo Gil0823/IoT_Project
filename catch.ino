aa069cd2bc258546febee7ba9e75dc7780333f5c
fact0823 <jun00400@naver.com>
2017-11-08 03:47:49 +0900

    build completely!!
    - 데이터 송/수신
    - 수신 데이터 해석
    - 액추에이터
    - 명령어구현
    (TODO)
    - 안전모드 제작

--- catch.ino
+++ catch.ino
@@ -1,9 +1,22 @@
 #include <SoftwareSerial.h>
 #include <DS1302.h>
 #include <DHT11.h>
-#include <stdio.h>
-#include <stdarg.h>
-#define BUF_SIZE 256     
+//#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define BUF_SIZE 16
+#define COMMEND 0
+#define START "start"
+#define GOWORK "read"
+#define CONTROL1 "relay1"
+#define CONTROL2 "relay2"
+#define CONTROL3 "relay3"
+#define CONTROL4 "relay4"
+#define DHT_H 0
+#define DHT_T 1
+#define SOUND 2
+#define BRIGHT 3
+#define PIR 4   
 
 const int relay_pin[4] = {2, 3, 4, 5};
 const int RST_pin = 6;
@@ -15,6 +28,7 @@ const int BTSerial_TX = 11;
 const int sound_pin = 12;
 const int temp_pin = 13;
 const int bright_pin = A0;
+const int senserCommendList[5] = {DHT_H, DHT_T, SOUND, BRIGHT, PIR};
 int error_DHT11 = 0;  // 온도센서 오류 여부
 int bright = 0; // 밝기정도
 float temp = 0.0;  // 온도
@@ -22,7 +36,6 @@ float humi = 0.0;  // 습도
 bool stat_PIR = false;  // 인체감지 여부
 bool sound_detect_stat = false;  // 소음감지 여부
 char buf[BUF_SIZE];
-
 
 typedef struct Relay {  // 릴레이 그룹
   int pin;  // 핀주소
@@ -47,11 +60,11 @@ DS1302 rtc(RST_pin, IO_pin, CLK_pin);  // RTC모듈 활성화
 DHT11 dht11(temp_pin);  // 온도센서(DHT11) 활성화
 
 void printTime();  // 시간 출력
-void sendData();  // 블루투스 모듈 송신
+void sendData(char *);  // 블루투스 모듈 송신
 bool recieveData();  // 블루투스 모듈 수신
-void readSenser(int x, ...);  // 센서값 수신
+char * readSenser(int);  // 센서값 수신
 void waitToTransmission();  // 데이터 수신 대기
-void translateData(char);
+void translateData();  // 데이터 해석
 
 Relay controller[4];
 
@@ -69,26 +82,6 @@ void setup() {
 
 void loop() {
   waitToTransmission();
-  bright = analogRead(bright_pin);
-  error_DHT11 = dht11.read(humi, temp);
-  if(digitalRead(PIR_pin) == HIGH) {
-    stat_PIR = true;
-  }
-  if(digitalRead(sound_pin) == HIGH) {
-    sound_detect_stat = true;
-  }
-  if(!(error_DHT11)) {  // DEBUG용도
-    Serial.print("temperature:");Serial.print(temp);
-    Serial.print(" humidity:");Serial.print(humi);
-    Serial.println();
-  }
-  else {
-    Serial.println();
-    Serial.print("Error No :");Serial.print(error_DHT11);
-    Serial.println();    
-  }
-  
-  
 }
 
 void printTime() {  
@@ -117,6 +110,62 @@ bool recieveData() {
   }
 }
 
+void sendData(char * data) {
+  BTSerial.write(data);
+}
+
+char * readSenser(int option) {
+  char dataOfBright[4];
+  char dataOfHumi[4];
+  char dataOfTemp[4];
+  char dataOfPir;
+  char dataOfSound;
+ 
+  bright = analogRead(bright_pin);
+  error_DHT11 = dht11.read(humi, temp);
+  if(digitalRead(PIR_pin) == HIGH) {
+    stat_PIR = true;
+    dataOfPir = '1';
+  }
+  if(digitalRead(sound_pin) == HIGH) {
+    sound_detect_stat = true;
+    dataOfSound = '1';
+  }
+  if(!(error_DHT11)) {  // DEBUG용도
+    Serial.print("temperature:");Serial.print(temp);
+    Serial.print(" humidity:");Serial.print(humi);
+    Serial.println();
+  }
+  else {
+    Serial.println();
+    Serial.print("Error No :");Serial.print(error_DHT11);
+    Serial.println();    
+  }
+  itoa(bright, dataOfBright, 10);
+  itoa((int)humi, dataOfHumi, 10);
+  itoa((int)temp, dataOfTemp, 10);
+
+  switch(option) {
+    case DHT_H:
+      return dataOfHumi;
+      break;
+    case DHT_T:
+      return dataOfTemp;
+      break;
+    case SOUND:
+      return dataOfSound;
+      break;
+    case BRIGHT:
+      return dataOfBright;
+      break;
+    case PIR:
+      return dataOfPir;
+      break;
+    default:
+      Serial.println("I think we have a problem in readSenser()");
+  }
+}
+
 void waitToTransmission() {
   static bool start = false;
   static int current = 0;
@@ -129,12 +178,9 @@ void waitToTransmission() {
     }
     else {
       if(start) {  
-        if(current-count != 0) {  // start값 변화 감지시 경주시작
-          current = 0;           // count는 수신받을시 값증가
-          count = 0;            // current는 상관없이 계속증가
-          start = false; 
-          break;                 
-        }                       
+        if(current-count != 0) {  // start값 변화 감지시 경주시작          
+          break;                 // count는 수신받을시 값증가            
+        }                       // current는 상관없이 계속증가 
         else {
           ;
         }
@@ -143,13 +189,84 @@ void waitToTransmission() {
     }
   }
   translateData();
+  current = 0;
+  count = 0;
+  start = false;
 }
 
 void translateData() {
-  ;
-}
-
-/*void readSenser() {
+  static int i = 0;
+  char data[BUF_SIZE];
+  int realSize = 0; 
+  char * senserData[4];
   
-}*/
+  for(i = 0; i < BUF_SIZE; i++) {
+    if(buf[i] == '\0') {
+      realSize = i-1;  // 크기로 연산(명령문자제외)
+      break;
+    }
+  }
+  for(i = 0; i < realSize; i++) {
+    data[i] = buf[i+1];
+  }
+
+  switch(buf[COMMEND]) {
+    case 'c':
+     if(!(strcmp(data, START))) {
+      Time t = rtc.time();
+      const String day = dayAsString(t.day);
+      char buf[50];
+  
+      snprintf(buf, sizeof(buf), "%s %04d-%02d-%02d %02d:%02d:%02d",
+           day.c_str(),
+           t.yr, t.mon, t.date,
+           t.hr, t.min, t.sec);
+      //sendData(buf);  // 시작시간을 마스터에게 송신
+     }
+     else if(!(strcmp(data, GOWORK))) {
+      for(i = 0; i < 5; i++) {
+        senserData[i] = readSenser(senserCommendList[i]);  // 4개의 센서로부터 데이터 수신
+      }                                                   
+      for(i = 0; i < 5; i++) {
+        sendData(senserData[i]);
+      }
+     }
+     else if(!(strcmp(data, CONTROL1))) {  
+      if(controller[0].stat == true) {
+        digitalWrite(controller[0].pin, LOW);
+      }
+      else {
+        digitalWrite(controller[0].pin, HIGH);
+      }
+     }
+     else if(!(strcmp(data, CONTROL2))) {  
+      if(controller[1].stat == true) {
+        digitalWrite(controller[1].pin, LOW);
+      }
+      else {
+        digitalWrite(controller[1].pin, HIGH);
+      }
+     }
+     else if(!(strcmp(data, CONTROL3))) {  
+      if(controller[2].stat == true) {
+        digitalWrite(controller[2].pin, LOW);
+      }
+      else {
+        digitalWrite(controller[2].pin, HIGH);
+      }
+     }
+     else if(!(strcmp(data, CONTROL4))) {  
+      if(controller[3].stat == true) {
+        digitalWrite(controller[3].pin, LOW);
+      }
+      else {
+        digitalWrite(controller[3].pin, HIGH);
+      }
+     }
+     break;
+    default:
+      Serial.println("I think we have a problem!");
+      Serial.print("Error code : "); Serial.print(buf[COMMEND]);
+  }
+}
 
